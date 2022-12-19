70728e7ba095e51e6db241416d6053e0746c060f
dldvlpr <dormoy.dev@gmail.com>
2022-12-19 21:35:25 +0100

    add validate and edit todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todoList, setTodoList] = useState([]);
+
+    function addTodo(content) {
+        const todo = { id: crypto.randomUUID(), done: false, edit: false, content };
+        setTodoList([...todoList, todo]);
+    }
+
+    function deleteTodo(id) {
+        setTodoList(todoList.filter((todo) => todo.id !== id));
+    }
+
+    function toggleTodo(id) {
+        setTodoList(
+            todoList.map((todo) =>
+                todo.id === id ? { ...todo, done: !todo.done } : todo
+            )
+        );
+    }
+
+    function toggleTodoEdit(id) {
+        setTodoList(
+            todoList.map((todo) =>
+                todo.id === id ? { ...todo, edit: !todo.edit } : todo
+            )
+        );
+    }
+
+    return (
+        <div className="d-flex justify-content-center align-items-center p-20">
+            <div className="card container p-20">
+                <h1 className="mb-20">Liste de tâches</h1>
+                <AddTodo addTodo={addTodo} />
+                <TodoList
+                    todoList={todoList}
+                    deleteTodo={deleteTodo}
+                    toggleTodo={toggleTodo}
+                    toggleTodoEdit={toggleTodoEdit}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default App;
